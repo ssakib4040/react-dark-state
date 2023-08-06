a08d0b663ad5b1ebce60407d66d778a637d8ed5a
Sadman Sakib <ssakib4040@gmail.com>
2023-08-06 15:15:52 +0600

    git add .

---
+++ src/index.d.ts
@@ -0,0 +1,8 @@
+// Define the return type of the useDarkState hook
+declare type DarkState = "dark" | "light" | null;
+
+// Declare the useDarkState custom hook
+declare function useDarkState(): DarkState;
+
+// Export the custom hook type
+export default useDarkState;
